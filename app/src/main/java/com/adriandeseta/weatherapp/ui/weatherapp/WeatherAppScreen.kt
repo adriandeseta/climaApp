2fb94d0d34658e5eea24868730f428be9f5db70e
adrian de seta <adriandeseta1985@gmail.com>
2025-07-30 18:58:27 -0300

    refactor delay bienvenida

--- app/src/main/java/com/adriandeseta/weatherapp/ui/weatherapp/WeatherAppScreen.kt
+++ app/src/main/java/com/adriandeseta/weatherapp/ui/weatherapp/WeatherAppScreen.kt
@@ -1,7 +1,16 @@
 package com.adriandeseta.weatherapp.ui.weatherapp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -15,23 +24,32 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.adriandeseta.weatherapp.R
 import com.adriandeseta.weatherapp.data.model.WeatherResponse
+import kotlinx.coroutines.delay
 
 
 @Composable
 fun WeatherAppScreen(viewModel: WeatherAppViewModel) {
     var city by remember { mutableStateOf("") }
     val state by viewModel.uiState.collectAsState()
+    var hasLaunched by remember { mutableStateOf(false) }
 
+    LaunchedEffect(Unit) {
+        delay(4000L) // 4 segundos
+    }
     Box(modifier = Modifier.fillMaxSize()) {
         LottieAnimationBackground(R.raw.default_weather)
 
@@ -111,7 +129,16 @@ fun WeatherAppScreen(viewModel: WeatherAppViewModel) {
                 }
                 when (state) {
                     is WeatherUiState.Loading -> {
-                        CircularProgressIndicator()
+                        Column(
+                            Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator(
+                                color = Color.White
+                            )
+                        }
+
                     }
 
                     is WeatherUiState.Success -> {
